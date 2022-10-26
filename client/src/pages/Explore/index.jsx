55f0d2b2415492f06ca1fb18a093b63fa9b7ed32
BerylBir <berylbir@gmail.com>
2022-10-25 23:13:39 -0700

    create filter components

--- client/src/pages/Explore/index.jsx
+++ client/src/pages/Explore/index.jsx
@@ -1,24 +1,20 @@
-import React, { useState } from "react";
-import DropdownFilter from "../../components/Filters/DropdownFilter";
+import React from "react";
 import PageLayout from "../../components/PageLayout";
+import ExploreFilter from "./components/ExploreFilter/ExploreFilter";
 
 function Explore() {
   const name = "Beryl";
-  const [distance, setDistance] = useState(null);
 
   return (
     <PageLayout>
       <h2>Welcome, {name} 👋</h2>
-      <p className="mt-4">Events from your groups</p>
-      <div>
-        <DropdownFilter
-          onSelect={(eventKey) => {
-            setDistance(eventKey);
-          }}
-          options={["1km", "2km", "Any"]}
-          name={distance === null ? "Distance" : distance}
-        />
-      </div>
+      <p className="mt-4">Events near you</p>
+      <ExploreFilter
+        onChange={(filtersState) => {
+          // TODO: Make api call to update filters for explore page
+          console.log(filtersState);
+        }}
+      />
     </PageLayout>
   );
 }
