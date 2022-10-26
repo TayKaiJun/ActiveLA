2171804ccf020b564318faf85d22ff11dbf4d93a
BerylBir <berylbir@gmail.com>
2022-10-25 21:01:18 -0700

    add dropdown filter component

--- client/src/pages/Explore/index.jsx
+++ client/src/pages/Explore/index.jsx
@@ -1,12 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import DropdownFilter from "../../components/Filters/DropdownFilter";
 import PageLayout from "../../components/PageLayout";
 
 function Explore() {
   const name = "Beryl";
+  const [distance, setDistance] = useState(null);
+
   return (
     <PageLayout>
       <h2>Welcome, {name} 👋</h2>
       <p className="mt-4">Events from your groups</p>
+      <div>
+        <DropdownFilter
+          onSelect={(eventKey) => {
+            setDistance(eventKey);
+          }}
+          options={["1km", "2km", "Any"]}
+          name={distance === null ? "Distance" : distance}
+        />
+      </div>
     </PageLayout>
   );
 }
