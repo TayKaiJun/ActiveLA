dcd780409f4c1b05f33e421b28b886bfd4c2bfdc
Lim Zhineng <limzhineng@Lims-MacBook-Pro.local>
2022-11-01 20:25:11 -0700

    Added Post Event Header

---
+++ client/src/pages/Post-Event/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PageLayout from "../../components/PageLayout";
+
+function postEvent() {
+  return (
+    <PageLayout>
+      <h2>List of Activities</h2>
+    </PageLayout>
+  );
+}
+
+export default postEvent;
