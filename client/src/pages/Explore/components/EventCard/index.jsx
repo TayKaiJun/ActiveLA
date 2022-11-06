eab34ecadcb5d895959ef5be31820f3a306e146e
BerylBir <berylbir@gmail.com>
2022-11-05 22:09:33 -0700

    run format

--- client/src/pages/Explore/components/EventCard/index.jsx
+++ client/src/pages/Explore/components/EventCard/index.jsx
@@ -1,21 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
-import Container from 'react-bootstrap/esm/Container';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Button from 'react-bootstrap/Button';
+import Container from "react-bootstrap/esm/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
 import axios from "axios";
-import Modal from 'react-bootstrap/Modal';
-import badmintonImage from "./badminton.jpg"
-import getAllEvents from '../../../../services/event-service';
+import Modal from "react-bootstrap/Modal";
+import badmintonImage from "./badminton.jpg";
+import getAllEvents from "../../../../services/event-service";
 
-
-function ModalMoreDetails({props}) {
-
+function ModalMoreDetails({ props }) {
   console.log(props);
 
-  const {id, name, date, time, location, skilllevel, agegroup, numberofplayers, costs} = props;
+  const { id, name, date, time, location, skilllevel, agegroup, numberofplayers, costs } = props;
 
   const [show, setShow] = useState(false);
 
@@ -24,19 +22,17 @@ function ModalMoreDetails({props}) {
 
   return (
     <>
-
       <div className="col text-center">
-      <Button variant="outline-primary" onClick={handleShow}>
-        Find Out More
-      </Button>
+        <Button variant="outline-primary" onClick={handleShow}>
+          Find Out More
+        </Button>
       </div>
-      
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Badminton</Modal.Title>
         </Modal.Header>
-        <img className="img-responsive" src={badmintonImage} alt=""/>
+        <img className="img-responsive" src={badmintonImage} alt="" />
         <Modal.Body>
           <ListGroup className="list-group-flush">
             <ListGroup.Item>Date: {date}</ListGroup.Item>
@@ -62,51 +58,45 @@ function ModalMoreDetails({props}) {
 }
 
 function EventCard() {
-  const [events, getEvents] = useState('');
-  
-  useEffect(() => {
-    getAllEvents().then((response) => {
-      const allEvents = response.data.Event;
-      getEvents(allEvents);
-    }
-    )
-    .catch((err) => {
-      console.log(err.message);
-    })
-  },[])
+  const [events, getEvents] = useState("");
 
-  console.log(events)
+  useEffect(() => {
+    getAllEvents()
+      .then((response) => {
+        const allEvents = response.data.Event;
+        getEvents(allEvents);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
+
+  console.log(events);
 
   return (
     <Row xs={1} md={4} className="g-4">
-      {
-      
-      events ? 
-
-      events.map((event, idx) => (
-        <Col>
-          <Card>
-            <Card.Img variant="top" src={badmintonImage} />
-            <Card.Body>
-              <Card.Title>{event.name}</Card.Title>
-              <ListGroup className="list-group-flush">
-                <ListGroup.Item>Date: {event.date}</ListGroup.Item>
-                <ListGroup.Item>Time: {event.time}</ListGroup.Item>
-                <ListGroup.Item>Location: {event.location}</ListGroup.Item>
-              </ListGroup>
-              <ModalMoreDetails props={event}/>
-            </Card.Body>
-          </Card>
-        </Col>
-      )
-      )
-      : <Container/>
-
-    }
+      {events ? (
+        events.map((event, idx) => (
+          <Col>
+            <Card>
+              <Card.Img variant="top" src={badmintonImage} />
+              <Card.Body>
+                <Card.Title>{event.name}</Card.Title>
+                <ListGroup className="list-group-flush">
+                  <ListGroup.Item>Date: {event.date}</ListGroup.Item>
+                  <ListGroup.Item>Time: {event.time}</ListGroup.Item>
+                  <ListGroup.Item>Location: {event.location}</ListGroup.Item>
+                </ListGroup>
+                <ModalMoreDetails props={event} />
+              </Card.Body>
+            </Card>
+          </Col>
+        ))
+      ) : (
+        <Container />
+      )}
     </Row>
   );
 }
-
-
 
 export default EventCard;
