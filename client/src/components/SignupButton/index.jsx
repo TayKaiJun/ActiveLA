0a84a34921d32f19509cb7040b71104616d8a3ed
TayKaiJun <tehohno@gmail.com>
2022-11-06 19:38:25 -0800

    Update signup form
    Touched up on validation & started implementing interest selection section

--- client/src/components/SignupButton/index.jsx
+++ client/src/components/SignupButton/index.jsx
@@ -2,13 +2,38 @@ import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import Form from "react-bootstrap/Form";
+import Toast from "react-bootstrap/Toast";
+import InputGroup from "react-bootstrap/InputGroup";
+import Dropdown from "react-bootstrap/Dropdown";
+import { Chip } from "primereact/chip";
 import { createNewUser } from "../../services/user-service";
 
 function SignupButton(props) {
-
   const [modalShow, setModalShow] = useState(false);
+  const [toastShow, setToastShow] = useState(false);
   const handleClose = () => setModalShow(false);
   const handleShow = () => setModalShow(true);
+
+  const sports = [
+    "Badminton",
+    "Basketball",
+    "Baseball",
+    "Cricket",
+    "Hockey",
+    "Soccer",
+    "Rugby",
+    "Football",
+    "Ultimate",
+    "Volleyball",
+    "Swimming",
+  ];
+
+  const proficiency = {
+    0: "No Experience",
+    1: "Beginner",
+    2: "Intermediate",
+    3: "Expert",
+  };
 
   const [data, setData] = useState({
     username: "",
@@ -16,7 +41,11 @@ function SignupButton(props) {
     name: "",
     pronouns: "",
     about: "",
-    interests: "",
+    interests: [
+      // TODO: Remove placeholder
+      { sport: "Soccer", level: 3 },
+      { sport: "Baseball", level: 2 },
+    ],
     password: "",
     confirmPassword: "",
   });
@@ -33,16 +62,11 @@ function SignupButton(props) {
       [id]: value,
     }));
 
-    if (data.password && data.confirmPassword && (data.password !== data.confirmPassword)) {
-      if (id === "password") {
-        setError(() => ({
-          [id]: "Passwords do not match!",
-        }));
-      }
-      else{
-        setError(() => ({
-          [id]: "",
-        }));
+    if (data.password && data.confirmPassword && (id === "password" || id === "confirmPassword")) {
+      if (data.password !== data.confirmPassword) {
+        setError(() => ({ [id]: "Passwords do not match!" }));
+      } else {
+        setError(() => ({ [id]: "" }));
       }
     }
   };
@@ -53,7 +77,14 @@ function SignupButton(props) {
     createNewUser(data)
       .then((res) => {
         if (res.success) {
-          // TODO: Toast that indicates 
+          // TODO: Make the Toast Popup showup
+
+          <Toast onClose={() => setToastShow(false)} show={toastShow} delay={3000} autohide>
+            <Toast.Header>
+              <strong className="me-auto">Sign-up Status</strong>
+            </Toast.Header>
+            <Toast.Body>Woohoo, you&apos;ve successfully signed up!</Toast.Body>
+          </Toast>;
         }
         setData({});
         handleClose();
@@ -63,7 +94,57 @@ function SignupButton(props) {
       });
   };
 
-  // TODO: Change default value to placeholders instead
+  const updateInterests = (e) => {
+    console.log(e);
+  };
+
+  // TODO: Figure this shit out
+  
+  // const interestForm = (
+  //   <InputGroup>
+  //     <Dropdown onSelect={(eventKey) => {
+  //             setData((prevState) => ({
+  //               ...prevState,
+  //               interests: [],
+  //             }}>
+  //       {sports.map((sportName) => {
+  //           return <option value={sportName}>{sportName}</option>;
+  //         })}
+  //       </Dropdown>
+  //       <Dropdown id="proficiency">
+  //         {proficiency.map((skills) => {
+  //           const { level, description } = skills;
+  //           return <option value={level}>{description}</option>;
+  //         })}
+  //     </Dropdown>
+  //     <Button onClick={updateInterests}>Add</Button>
+  //   </InputGroup>
+  // );
+
+  const renderInterests = data.interests
+    ? data.interests.map((interest) => {
+        const { sport, level } = interest;
+        const labelText = `${sport} (${level})`;
+        return (
+          <div style={{ marginLeft: "8px", marginTop: "10px", display: "inline-block" }}>
+            <Chip
+              label={labelText}
+              removable // TODO: Debug the remove button
+              onRemove={() => {
+                const newInterestArray = data.interests.filter((remainingInterest) => {
+                  return remainingInterest.sport !== sport;
+                });
+                console.log(newInterestArray);
+                setData((prevState) => ({
+                  ...prevState,
+                  interests: newInterestArray,
+                }));
+              }}
+            />
+          </div>
+        );
+      })
+    : null;
 
   return (
     <>
@@ -78,25 +159,28 @@ function SignupButton(props) {
           <Form onSubmit={onFormSubmit}>
             <Form.Group className="mb-2" controlId="username">
               <Form.Label>Username</Form.Label>
-              <Form.Control type="text" autoFocus onChange={onInput} />
+              <Form.Control type="text" autoFocus onChange={onInput} placeholder="Create username" required />
             </Form.Group>
 
             <Form.Group className="mb-2" controlId="email">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" onChange={onInput} />
+              <Form.Control type="email" onChange={onInput} placeholder="Enter a valid UCLA email" required />
             </Form.Group>
 
             <Form.Group className="mb-2" controlId="name">
               <Form.Label>Name</Form.Label>
-              <Form.Control type="text" onChange={onInput} />
+              <Form.Control type="text" onChange={onInput} placeholder="Enter your name" required />
             </Form.Group>
 
             <Form.Group className="mb-2" controlId="pronouns">
               <Form.Label>Pronouns</Form.Label>
-              <Form.Check type="radio" name="group1" id="signup_pronouns-1" label="he/him" />
-              <Form.Check type="radio" name="group1" id="signup_pronouns-2" label="she/her" />
-              <Form.Check type="radio" name="group1" id="signup_pronouns-3" label="they/them" />
-              <Form.Check type="radio" name="group1" id="signup_pronouns-4" label="No preference" />
+              <Form.Control as="select" onChange={onInput} placeholder="Select your pronouns">
+                <option value="Do not wish to declare">Select your pronouns</option>
+                <option value="He/Him">He/Him</option>
+                <option value="She/Her">She/Her</option>
+                <option value="They/Them">They/Them</option>
+                <option value="Others">Others</option>
+              </Form.Control>
             </Form.Group>
 
             <Form.Group className="mb-2" controlId="about">
@@ -109,27 +193,36 @@ function SignupButton(props) {
               />
             </Form.Group>
 
-            <Form.Group className="mb-2" controlId="interests">
-              <Form.Label>Interest</Form.Label>
-              {/* 
-                TODO: 
-                  - In each row, have a dropdown list that user can select interest from
-                  - have a slider or a row of radio buttons to select proficiency level
-                  - Have a button that creates a new row of entry to add more interests
-               */}
-              <Form.Control type="text" defaultValue="Indicate interests" disabled />
+            <Form.Group className="mb-2">
+              <Form.Label>Interests & Proficiency Level</Form.Label>
+              <InputGroup>
+                {/* {interestForm} */}
+                {/* <Form.Select>
+                  <option value="Badminton">Badminton</option>
+                  <option value="Basketball">Basketball</option>
+                  <option value="Soccer">Soccer</option>
+                  <option value="Volleyball">Volleyball</option>
+                </Form.Select>
+                <Form.Select>
+                  <option value="0">No experience</option>
+                  <option value="1">Beginner</option>
+                  <option value="2">Intermediate</option>
+                  <option value="3">Expert</option>
+                </Form.Select>
+                <Button onClick={updateInterests}>Add</Button> */}
+              </InputGroup>
+              <div>{renderInterests}</div>
             </Form.Group>
 
             <Form.Group className="mb-2" controlId="password">
               <Form.Label>Create Password</Form.Label>
-              {/* TODO: remmove default value */}
-              <Form.Control type="password" defaultValue="helloworld" onChange={onInput} required />
+              <Form.Control type="password" onChange={onInput} placeholder="Create a password" required />
               {formErrors.password && <p className="text-danger">{formErrors.password}</p>}
             </Form.Group>
 
             <Form.Group className="mb-2" controlId="confirmPassword">
-              <Form.Label>Re-enter Password</Form.Label>
-              <Form.Control type="password" onChange={onInput} required />
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control type="password" onChange={onInput} placeholder="Please re-enter password" required />
               {formErrors.confirmPassword && <p className="text-danger">{formErrors.confirmPassword}</p>}
             </Form.Group>
 
