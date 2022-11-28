8951761eb31a3a0c70fa7a60b5581a40cf5bbcb4
Pierce Chong <48197758+PierceCCH@users.noreply.github.com>
2022-11-28 15:12:46 -0800

    restyled login and signup page

--- client/src/components/FormLayout/index.jsx
+++ client/src/components/FormLayout/index.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import Button from 'react-bootstrap/esm/Button';
 import Container from 'react-bootstrap/esm/Container';
+import { useNavigate } from 'react-router-dom';
 
 function FormLayout(props) {
   const { children } = props;
+  const navigate = useNavigate();
+
   return (
-    <Container className="mt-6 px-6">{children}</Container>
+    <>
+      <Button style={{margin: "2rem 0 8rem 2rem", color: "black", backgroundColor: "white", border: "none"}} onClick={() => navigate('/')}>
+        <h1>
+          ActiveLA
+        </h1>
+      </Button>
+      <Container style={{maxWidth: "48rem"}}>
+        {children}
+      </Container>
+    </>
   )
 }
 
