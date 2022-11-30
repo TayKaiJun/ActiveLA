c3759bb0a815024a1feccd6d93e26d647740c425
BerylBir <berylbir@gmail.com>
2022-11-29 21:19:04 -0800

    add loading spinner

--- client/src/pages/Explore/components/Events/index.jsx
+++ client/src/pages/Explore/components/Events/index.jsx
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import Container from "react-bootstrap/esm/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import { ProgressSpinner } from "primereact/progressspinner";
 import EventCard from "../../../../components/EventCard";
+import PageStatus from "../../../../global/page-status";
 
 function Events(props) {
   const { events, requestJoinHandler } = props;
+  const pageStatus = useContext(PageStatus);
 
   return (
-    <Row xs={1} md={4} className="g-4">
-      {events ? (
-        events.map((event) => (
-          <Col>
-            <EventCard event={event} requestJoinHandler={requestJoinHandler}/>
-          </Col>
-        ))
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexGrow: 1,
+      }}
+    >
+      {pageStatus.isLoading ? (
+        <ProgressSpinner />
       ) : (
-        <Container />
+        <Row xs={1} md={4} className="g-4">
+          {events ? (
+            events.map((event) => (
+              <Col>
+                <EventCard event={event} requestJoinHandler={requestJoinHandler} />
+              </Col>
+            ))
+          ) : (
+            <Container />
+          )}
+        </Row>
       )}
-    </Row>
+    </div>
   );
 }
 
