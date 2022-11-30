acb2d00eda230a95c569044f2b85741c47b68a4e
ZnPR0N00BnZ <limzhineng@hotmail.com>
2022-11-29 23:17:03 -0800

    Implemented Loading Indicator in My Events Page

--- client/src/pages/MyEvents/components/hostingDisplay/index.jsx
+++ client/src/pages/MyEvents/components/hostingDisplay/index.jsx
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import Container from "react-bootstrap/esm/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import { ProgressSpinner } from "primereact/progressspinner";
 import HostingCard from "../hostingCard";
+import PageStatus from "../../../../global/page-status";
 
 function HostingEvents(props) {
   const { events } = props;
+  const pageStatus = useContext(PageStatus);
 
   return (
-    <Row xs={1} md={4} className="g-4">
-      {events ? (
-        events.map((event) => (
-          <Col>
-            <HostingCard event={event} />
-          </Col>
-        ))
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexGrow: 1,
+      }}
+    >
+      {pageStatus.isLoading ? (
+        <ProgressSpinner />
       ) : (
-        <Container />
+        <Row xs={1} md={4} className="g-4">
+          {events ? (
+            events.map((event) => (
+              <Col>
+                <HostingCard event={event} />
+              </Col>
+            ))
+          ) : (
+            <Container />
+          )}
+        </Row>
       )}
-    </Row>
+    </div>
   );
 }
 
--- client/src/pages/MyEvents/components/pendingDisplay/index.jsx
+++ client/src/pages/MyEvents/components/pendingDisplay/index.jsx
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { useContext } from "react";
 import Container from "react-bootstrap/esm/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import { ProgressSpinner } from "primereact/progressspinner";
 import PendingCard from "../pendingCard";
+import PageStatus from "../../../../global/page-status";
 
 function PendingEvents(props) {
   const { events } = props;
+  const pageStatus = useContext(PageStatus);
 
   return (
-    <Row xs={1} md={4} className="g-4">
-      {events ? (
-        events.map((event) => (
-          <Col>
-            <PendingCard event={event} />
-          </Col>
-        ))
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexGrow: 1,
+      }}
+    >
+      {pageStatus.isLoading ? (
+        <ProgressSpinner />
       ) : (
-        <Container />
+        <Row xs={1} md={4} className="g-4">
+          {events ? (
+            events.map((event) => (
+              <Col>
+                <PendingCard event={event} />
+              </Col>
+            ))
+          ) : (
+            <Container />
+          )}
+        </Row>
       )}
-    </Row>
+    </div>
   );
 }
 
