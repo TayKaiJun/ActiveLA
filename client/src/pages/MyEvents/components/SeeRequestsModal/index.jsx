9b7b882b9660dcfc6a4d21bf011363363bd61769
ZnPR0N00BnZ <limzhineng@hotmail.com>
2022-11-29 22:42:14 -0800

    Refactoring in My-Events folder

--- client/src/pages/MyEvents/components/SeeRequestsModal/index.jsx
+++ client/src/pages/MyEvents/components/SeeRequestsModal/index.jsx
@@ -25,9 +25,6 @@ function SeeRequestsModal(props) {
           <Button variant="secondary" onClick={handleClose}>
             Close
           </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
         </Modal.Footer>
       </Modal>
     </>
